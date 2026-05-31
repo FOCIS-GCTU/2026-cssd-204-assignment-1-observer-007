9bcbaff42e7e9d0fd9f63a360a78135bd8c454d3
observer-007 <2425400647@live.gctu.edu.gh>
2026-05-31 00:19:21 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Desmond Essel
+# Student ID: 2425400647
+# Email: mrrr.essel@gmail.com
+# Grader:
+#
+# On my honor, Desmond Essel, this programming assignment is my own work
+# and I have not provided this to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in small and large stylized block letters made up of the letters I, D, and E
 
+    print()
+    print("...IDE")
+    print()
+    print("...IIIIIIIIIIII........DDDDDDDD............EEEEEEEEEEEE.....")
+    print("......,.II.............DDDDDDDDDD..........EEEEEEEEEEEE.....")
+    print("........II.............DD.......DDD........EE...............")
+    print("........II.............DD........DD........EE...............")
+    print("........II.............DD........DD........EEEEEEEEEEEE.....")
+    print("........II.............DD........DD........EEEEEEEEEEEE.....")
+    print("........II.............DD........DD........EE...............")
+    print("........II.............DD.......DDD........EE...............")
+    print("........II........**...DDDDDDDDDD.....**...EEEEEEEEEEEE...**")
+    print("...IIIIIIIIIIII...**...DDDDDDDD.......**...EEEEEEEEEEEE...**")
+    print()
 
 
 main()
