c77f9da8a2917797da5cb6bfae2078c708809de2
observer-007 <2425400647@live.gctu.edu.gh>
2026-05-31 00:14:01 +0000

    Update initials_test.py

--- initials_test.py
+++ initials_test.py
@@ -1,14 +1,33 @@
-import os.path
-import sys
-from initials import main
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Desmond Essel
+# Student ID: 2425400647
+# Email: mrrr.essel@gmail.com
+# Grader:
+#
+# On my honor, Desmond Essel, this programming assignment is my own work
+# and I have not provided this to any other student.
 
 
-def test_initials():
-    try:
-        exists = os.path.exists("initials.py")
-        assert exists == True
-    except:
-        sys.exit()
+def main():
+    # Print initials in small and large stylized block letters made up of the letters I, D, and E
+
+    print()
+    print("...IDE")
+    print()
+    print("...IIIIIIIIIIII........DDDDDDDD............EEEEEEEEEEEE.....")
+    print("......,.II.............DDDDDDDDDD..........EEEEEEEEEEEE.....")
+    print("........II.............DD.......DDD........EE...............")
+    print("........II.............DD........DD........EE...............")
+    print("........II.............DD........DD........EEEEEEEEEEEE.....")
+    print("........II.............DD........DD........EEEEEEEEEEEE.....")
+    print("........II.............DD........DD........EE...............")
+    print("........II.............DD.......DDD........EE...............")
+    print("........II........**...DDDDDDDDDD.....**...EEEEEEEEEEEE...**")
+    print("...IIIIIIIIIIII...**...DDDDDDDD.......**...EEEEEEEEEEEE...**")
+    print()
 
 
-    main()
+main()
